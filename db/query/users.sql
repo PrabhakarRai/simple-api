27a75ed42a10e5a24dc12056c00148aa6f7cf150
Prabhakar Rai <student343.bhu@gmail.com>
2021-03-12 11:34:41 +0530

    Update users.sql

--- db/query/users.sql
+++ db/query/users.sql
@@ -3,11 +3,14 @@ INSERT INTO users (username, name) VALUES ($1, $2) RETURNING id;
 
 -- name: GetUserByUsername :one
 SELECT * FROM users
-WHERE username = $1;
+WHERE username = $1 LIMIT 1;
 
 -- name: GetUserByID :one
 SELECT * FROM users
-WHERE id = $1;
+WHERE id = $1 LIMIT 1;
 
 -- name: DeleteUser :exec
 DELETE FROM users WHERE id = $1;
+
+-- name: UpdateUsername :exec
+UPDATE users SET name = $2 WHERE id = $1;
