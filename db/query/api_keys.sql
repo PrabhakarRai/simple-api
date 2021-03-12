6640672e5c0097d00ca15194c1c9870e51195382
Prabhakar Rai <student343.bhu@gmail.com>
2021-03-12 13:47:50 +0530

    fixed some queries

--- db/query/api_keys.sql
+++ db/query/api_keys.sql
@@ -1,15 +1,15 @@
 -- name: CreateAPIKey :one
-INSERT INTO api_keys (key, owner) VALUES ($1, $2) RETURNING id;
+INSERT INTO api_keys (key, owner) VALUES ($1, $2) RETURNING id, key, owner;
 
--- name: GetUserIDByAPIKey :one
+-- name: GetAPIKeyDetailsByKey :one
 SELECT * FROM api_keys
-WHERE owner = $1 LIMIT 1;
+WHERE key = $1 LIMIT 1;
 
--- name: GetAPIKeyByUserID :one
+-- name: GetAPIKeysByOwner :many
 SELECT * FROM api_keys
-WHERE api_keys.owner = $1;
+WHERE owner = $1;
 
--- name: GetAPIKeyByUsername :one
+-- name: GetAPIKeysByUsername :many
 SELECT * FROM api_keys
 WHERE api_keys.owner = (SELECT id FROM users WHERE users.username = $1 LIMIT 1);
 
@@ -20,4 +20,13 @@ DELETE FROM api_keys WHERE key = $1;
 DELETE FROM api_keys WHERE owner = $1;
 
 -- name: DeleteAPIKeysByUsername :exec
-DELETE FROM api_keys WHERE api_keys.owner = (SELECT id FROM users WHERE users.username = $1 LIMIT 1);
+DELETE FROM api_keys WHERE api_keys.owner = (SELECT id FROM users WHERE users.username = $1);
+
+-- name: UpdateAPIKeyEnabled :exec
+UPDATE api_keys SET enabled = $2 WHERE key = $1;
+
+-- name: UpdateAPIKeyHits :exec
+UPDATE api_keys SET hits = hits+1 WHERE key = $1;
+
+-- name: UpdateAPIKeyErrors :exec
+UPDATE api_keys SET errors = errors+1 WHERE key = $1;
